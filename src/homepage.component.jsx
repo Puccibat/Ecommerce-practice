317930c0fc990932273b096f0299f4e8863d3e76
Baptiste <baptiste.puccinelli@gmail.com>
2020-03-23 17:16:28 +0100

    Homepage layout

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>COLLIERS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>BRACELETS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>SAUTOIRES</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>BOUCLES D'OREILLES</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>BAGUES</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
